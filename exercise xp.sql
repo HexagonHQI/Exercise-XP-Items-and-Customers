0a1bfcb6be0e809746960dec00455a93d89a64e3
HexagonHQI <163999976+HexagonHQI@users.noreply.github.com>
2024-07-13 07:44:23 +0300

    Update exercise xp.sql

--- exercise xp.sql
+++ exercise xp.sql
@@ -49,3 +49,19 @@ SELECT * FROM items WHERE price <= 300;
 SELECT * FROM customers WHERE last_name = 'Smith';
 SELECT * FROM customers WHERE last_name = 'Jones';
 SELECT * FROM customers WHERE first_name != 'Scott';
+
+Correction:
+1) SELECT * 
+FROM items 
+ORDER BY price ASC;
+2)SELECT * 
+FROM items 
+WHERE price >= 80 
+ORDER BY price DESC;
+3)SELECT first_name, last_name, email, phone, address 
+FROM customers 
+ORDER BY first_name ASC 
+LIMIT 3;
+4)SELECT last_name 
+FROM customers 
+ORDER BY last_name DESC;
